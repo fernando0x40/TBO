ab4fb400696ad7d366bbbf40f49af5ad64c53907
Fernando Bisi Vieira <fernando0x40@gmail.com>
2023-03-28 10:49:06 -0300

    Implemented public methods.

--- eratostenes/eratostenes.cpp
+++ eratostenes/eratostenes.cpp
@@ -20,7 +20,8 @@ void eratostenes::mark_multiple(unsigned int number){
 }
 unsigned int eratostenes::next_prime(unsigned int current_prime){
   for (unsigned int i = current_prime + 1; !this->out_of_bounds(i); i++)
-    if (this->find(i)[0] == 0) return i;      
+    if (this->find(i)[0] == 0) return i;
+  return this->size+2;  
 }
 bool eratostenes::out_of_bounds(unsigned int number){
   return number > this->size+1;
@@ -33,12 +34,9 @@ void eratostenes::print_non_marked(){
   printf(" ]\n");  
 }
 void eratostenes::run(){
-  for (unsigned int i = 0; !this->out_of_bounds(i); i++)
-  {
-    /* code */
-  }
-  
+  for (unsigned int i = 2; !this->out_of_bounds(i); i = this->next_prime(i))
+    this->mark_multiple(i);
 }
 void eratostenes::print(){
-
+  this->print_non_marked();
 }
